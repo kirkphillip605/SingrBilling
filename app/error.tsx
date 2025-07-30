b6cf5572aacc24f2a9e5c8ebde34c626e407013a
Phillip Kirk <168415485+kirkphillip605@users.noreply.github.com>
2025-07-30 05:58:57 -0500

    Add 'use client' to All Required Components

--- app/error.tsx
+++ app/error.tsx
@@ -1,5 +1,7 @@
 'use client';
 
+'use client';
+
 import { useEffect } from 'react';
 import { Button } from '@/components/ui/button';
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card';
